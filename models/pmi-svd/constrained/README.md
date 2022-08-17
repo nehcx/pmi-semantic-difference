c27bfd8c75386afe8dfc217d36e4efd4aeac6d98
idiig <idiig@hotmail.com>
2022-08-17 19:06:34 +0900

    delete original construction

--- models/pmi-svd/constrained/README.md
+++
@@ -1,5 +0,0 @@
-# PMI-SVDc
-- model.py: main model
-- search.py: hyperparameter search
-- train.py: training one model
-- util.py: utils
--- preprocess/pseudowords/ioutils.py
+++
@@ -1,9 +0,0 @@
-import _pickle
-
-def write_pickle(data, filename):
-    fp = open(filename, 'wb')
-    _pickle.dump(data, fp)
-
-def load_pickle(filename):
-    fp = open(filename, 'rb')
-    return _pickle.load(fp)
